749be32f52aeac42aa630dc508348f4b8b941bca
moi3n <moi3nr@gmail.com>
2023-03-16 23:18:24 +0330

    added fully conv soilnet

--- soilnet/soil_net.py
+++ soilnet/soil_net.py
@@ -26,6 +26,31 @@ class SoilNet(nn.Module):
         output = self.reg(flat_raster,auxillary_data)
         return output
     
+class SoilNetFC(nn.Module):
+    def __init__(self, input_size=1024, aux_size=12, hidden_size=128):
+        super().__init__()
+        self.cnn = CNNFlattener()
+        self.reg = Regressor()
+        
+    def forward(self, raster_stack, auxillary_data):
+        """
+        Forward pass of the SoilNet module.
+        
+        Args:
+            raster_stack (torch.Tensor): Input tensor of shape (batch_size, channels, height, width).
+            auxillary_data (torch.Tensor): Auxiliary input tensor of shape (batch_size, aux_size).
+        
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, 1).
+        """
+        flat_raster = self.cnn(raster_stack)
+        output = self.reg(flat_raster,auxillary_data)
+        return output
+       
+    
+    
+    
+    
     
 if __name__ == "__main__":
     x = torch.randn((32,12,128,128))
@@ -33,3 +58,7 @@ if __name__ == "__main__":
     model = SoilNet()
     z = model(x,y)
     print(z.detach().shape)
+    
+    
+    
+    
