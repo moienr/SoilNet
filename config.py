711a3c022d80d90221ccb2283da3f39ed7f53456
moienr <moi3nr@gmail.com>
2024-08-19 19:25:06 +0330

    [Add] palceholder paths

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-train_l8_folder_path = 'D:\python\SoilNet-PreRelease\dataset\l8_images\\train\\'
-test_l8_folder_path = 'D:\python\SoilNet-PreRelease\dataset\l8_images\\test\\'
-val_l8_folder_path = 'D:\python\SoilNet-PreRelease\dataset\l8_images\\val\\'
-lucas_csv_path = 'D:\python\SoilNet-PreRelease\dataset\LUCAS_2015_all.csv'
-climate_csv_folder_path = "D:\\python\\SoilNet-PreRelease\\dataset\\Climate\\All\\filled\\"
-
-SIMCLR_PATH = "D:\\python\\SoilNet\\results\\RUN_LUCAS_Self560_ViT_Trans_D_2024_08_19_T_16_13_SelfSupervised.pth"
+train_l8_folder_path = '/path/to/your/project/dataset/l8_images/train/'
+test_l8_folder_path = '/path/to/your/project/dataset/l8_images/test/'
+val_l8_folder_path = '/path/to/your/project/dataset/l8_images/val/'
+lucas_csv_path = '/path/to/your/project/dataset/LUCAS_2015_all.csv'
+climate_csv_folder_path = "/path/to/your/project/dataset/Climate/All/filled/"
+SIMCLR_PATH = "/path/to/your/project/results/RUN_LUCAS_Self560_ViT_Trans_D_2024_08_19_T_16_13_SelfSupervised.pth"
