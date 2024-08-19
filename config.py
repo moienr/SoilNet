f96a488fb99a85b24aba02fb355253d7924bb5ee
moienr <moi3nr@gmail.com>
2024-08-19 18:56:35 +0330

    [Add] simclr path

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ test_l8_folder_path = 'D:\python\SoilNet-PreRelease\dataset\l8_images\\test\\'
 val_l8_folder_path = 'D:\python\SoilNet-PreRelease\dataset\l8_images\\val\\'
 lucas_csv_path = 'D:\python\SoilNet-PreRelease\dataset\LUCAS_2015_all.csv'
 climate_csv_folder_path = "D:\\python\\SoilNet-PreRelease\\dataset\\Climate\\All\\filled\\"
+
+SIMCLR_PATH = "D:\\python\\SoilNet\\results\\RUN_LUCAS_Self560_ViT_Trans_D_2024_08_19_T_16_13_SelfSupervised.pth"
